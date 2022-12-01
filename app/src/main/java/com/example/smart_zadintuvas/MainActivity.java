3c8ae31a5255fe3d6ab1c44449bc9b09c7795dd6
Adomas Gurauskas <adomas22@gmail.com>
2022-12-01 20:19:12 +0200

    0.3
    -Sutvarkytas CountDown nustačius žadintuvą.
    -Implementuotas naujas WeatherActivity, pasiekiamas mygtuku.
    -Implementuotas vartotojo lokacijos gavimas.
    -Implementuotas orų informacijos gavimas naudojant OpenWeather programines sąsajas GeocodingAPI ir Current Weather Data

--- app/src/main/java/com/example/smart_zadintuvas/MainActivity.java
+++ app/src/main/java/com/example/smart_zadintuvas/MainActivity.java
@@ -1,30 +1,42 @@
 package com.example.smart_zadintuvas;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.appcompat.widget.Toolbar;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
+    CountDownTimer myCountDownTimer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+
         Button setAlarmButton = (Button) findViewById(R.id.button2);
         setAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,11 +45,21 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
                 timePicker.show(getSupportFragmentManager(), "time picker");
             }
         });
+
         Button cancelAlarmButton = (Button) findViewById(R.id.button3);
         cancelAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cancelAlarm();
+            }
+        });
+
+        Button weatherButton = (Button) findViewById(R.id.button4);
+        weatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), WeatherActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -57,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         else minutes = "" + minute;
 
         TextView textView = findViewById(R.id.textView2);
-        textView.setText("Hour: " + valandos + " Minute: " + minutes);
+        //textView.setText("Hour: " + valandos + " Minute: " + minutes);
         Toast toast = Toast.makeText(getApplicationContext(), "Alarm created for " + valandos + ":" + minutes, Toast.LENGTH_SHORT);
         toast.show();
 
@@ -66,28 +88,57 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         c.set(Calendar.MINUTE, minute);
         c.set(Calendar.SECOND, 0);
 
+        myCountDownTimer = skaiciuojaLaika(c, textView);
+        myCountDownTimer.start();
         startAlarm(c);
     }
-    private void startAlarm(Calendar c)
-    {
+
+    private void startAlarm(Calendar c) {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlarmBroadcastReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
 
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+
     }
-    private void cancelAlarm()
-    {
+
+    private void cancelAlarm() {
         TextView textView = findViewById(R.id.textView2);
         textView.setText("No alarm set");
         Toast toast = Toast.makeText(getApplicationContext(), "Alarm cancelled", Toast.LENGTH_SHORT);
         toast.show();
+
+        if (myCountDownTimer != null)
+            myCountDownTimer.cancel();
+
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlarmBroadcastReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
 
         alarmManager.cancel(pendingIntent);
+
+
     }
 
+    public CountDownTimer skaiciuojaLaika(Calendar c, TextView textView) {
+        CountDownTimer counter = new CountDownTimer(c.getTimeInMillis() - System.currentTimeMillis(), 1000) {
+            public void onTick(long millisUntilFinished) {
+                NumberFormat f = new DecimalFormat("00");
+                long hour = (millisUntilFinished / 3600000) % 24;
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+                textView.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
+            }
+
+            public void onFinish() {
+                textView.setText("No Alarm Set");
+            }
+
+        };
+        return counter;
+    }
+    
 }
+
+
---
+++ app/src/main/java/com/example/smart_zadintuvas/WeatherActivity.java
@@ -0,0 +1,176 @@
+package com.example.smart_zadintuvas;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.icu.text.DecimalFormat;
+import android.os.Bundle;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.view.View;
+import android.widget.TextView;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class WeatherActivity extends AppCompatActivity implements LocationListener {
+    protected LocationManager locationManager;
+    TextView txtLat;
+    TextView txtJSON;
+    private final String url = "http://api.openweathermap.org/geo/1.0/reverse";
+    private final String url2 = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appID = "250fa86bc13f410ed32092cfcdcc92cb";
+    public final DecimalFormat df = new DecimalFormat("#.#");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    0);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,
+                5, this);
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLat = (TextView) findViewById(R.id.locationtextview);
+        double lat  = round(location.getLatitude(), 2);
+        double longt = round(location.getLongitude(), 2);
+        txtLat.setText("Latitude: " + lat + ", Longitude: " + longt);
+        gaunaMiesta(lat, longt);
+    }
+
+    public void gaunaMiesta(double lat, double longt)
+    {
+        String tempUrl = "";
+        String latitude = "" + lat;
+        String longtitude = "" + longt;
+
+        tempUrl = url + "?lat=" + latitude + "&lon=" + longtitude + "&appid=" + appID;
+        Log.d("url: ", tempUrl);
+        StringRequest jsonObjectRequest = new StringRequest
+                (Request.Method.GET, tempUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null) {
+                            Log.d("response", response);
+                            try {
+                                JSONArray jsonResponse = new JSONArray(response);
+                                JSONObject json = jsonResponse.getJSONObject(0);
+                                String miestas = json.getString("name");
+                                Log.d("miestas: ", miestas);
+                                String salis = json.getString("country");
+                                Log.d("salis: ", salis);
+                                gaunaOrus(miestas, salis);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(jsonObjectRequest);
+    }
+
+    private void gaunaOrus(String name, String salis){
+        String tempUrl = "";
+        tempUrl = url2 + "?q=" + name + "," + salis + "&appid=" + appID;
+        Log.d("url2: ", tempUrl);
+        StringRequest jsonObjectRequest = new StringRequest
+                (Request.Method.GET, tempUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null) {
+                            String output = "";
+                            Log.d("response2", response);
+                            apdorotiOrus(response, name, salis);
+                            }
+                        }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(jsonObjectRequest);
+    }
+
+    public static double round(double value, int places) {
+        if (places > 0) {
+            long factor = (long) Math.pow(10, places);
+            value = value * factor;
+            long tmp = Math.round(value);
+            return (double) tmp / factor;
+        }
+        else return 0;
+    }
+
+    public void apdorotiOrus(String response, String miestas, String salis)
+    {
+        String output = "";
+        try {
+            JSONObject jsonResponse = new JSONObject(response);
+            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+            JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+            String main = jsonObjectWeather.getString("main");
+            String description = jsonObjectWeather.getString("description");
+            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+            double temp = jsonObjectMain.getDouble("temp") - 273.15;
+            double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+            output += " Current weather of " + miestas + " (" + salis + ")"
+                    + "\n Temp: " + df.format(temp) + " °C"
+                    + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                    + "\n Weather: " + main
+                    + "\n Description: " + description;
+            txtJSON = findViewById(R.id.jsontextView);
+            txtJSON.setText(output);
+            txtJSON.setVisibility(View.VISIBLE);
+            txtLat.setVisibility(View.VISIBLE);
+            findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
+
